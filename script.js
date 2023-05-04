331f55f7be7bcc27a17c0550babe46a8466f7c44
Vivek Sharma <119029794+vivek-sharmagit@users.noreply.github.com>
2023-05-04 19:11:00 +0530

    Added selectors, event listeners and functionality

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 // Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 // Event Listeners
+todoButton.addEventListener("click", addTodo);
+
 
 // Functions
+function addTodo (event){
+    //Prevent form from submitting
+    event.preventDefault();
+
+    // Creating Todo Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add('todo')
+
+    // Creating LI for list of todo
+    const newTodo = document.createElement("div");
+    newTodo.classList.add('todo-item');
+    newTodo.innerText = 'hey';
+    todoDiv.appendChild(newTodo);
+
+    //Check Mark Button
+    const completedButton =document.createElement('button');
+    completedButton.classList.add('complete-btn');
+    completedButton.innerHTML ='<i class="fa-solid fa-check"></i>';
+    todoDiv.appendChild(completedButton)
+
+    //Check Trash Button
+    const trashButton =document.createElement('button');
+    trashButton.classList.add('complete-btn');
+    trashButton.innerHTML ='<i class="fa-solid fa-trash"></i>';
+    todoDiv.appendChild(trashButton)
+
+
+
+    //Append todo to list
+    todoList.appendChild(todoDiv);
+};
+
